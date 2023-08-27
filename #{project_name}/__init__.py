5845891f40e1f172746fb6d21c4ee4cf4315c44e
robo <robo2020@gmail.com>
2023-08-27 23:29:58 +0300

    add logging

--- #{project_name}/__init__.py
+++ #{project_name}/__init__.py
@@ -1,1 +1,12 @@
+import logging.config
+import pathlib
+import json
+
+
 __version__ = "0.0.1"
+
+config_file = pathlib.Path(__file__).parents[0].joinpath("logging.json")
+with open(config_file, "r", encoding="utf-8") as f:
+    logging_config = json.load(f)
+
+logging.config.dictConfig(logging_config)
