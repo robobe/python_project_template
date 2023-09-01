f26b9a8c57a940f1163279a654ee0795ec795914
robo <robo2020@gmail.com>
2023-09-01 16:16:22 +0300

    vscode extensions install script

--- scripts/install_vs_code_extensions.bash
+++ scripts/install_vs_code_extensions.bash
@@ -1,3 +1,16 @@
-
+#project manager
+code --install-extension alefragnani/vscode-project-manager
+# auto docstring
 code --install-extension njpwerner.autodocstring
-
+# tabout
+code --install-extension albert.TabOut
+# git graph
+code --install-extension mhutchie.git-graph
+# Tasks
+code --install-extension actboy168.tasks
+# Markdown All in One
+code --install-extension yzhang.markdown-all-in-one
+# Markdown preview
+code --install-extension shd101wyy.markdown-preview-enhanced
+# spell check
+code --install-extension streetsidesoftware.code-spell-checker
